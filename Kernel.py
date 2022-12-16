3bc02e593fa1dc830c342e016fe95a0f6097cb6e
Sir-Sorg <sinaorojlo53@gmail.com>
2022-12-16 21:15:33 +0330

    creating object and class

--- Kernel.py
+++ Kernel.py
@@ -1,7 +1,7 @@
 import csv
 
 
-def get_input(adrs):
+def get_input(adrs: str) -> list:
     data = list()
     with open(adrs) as csvfile:
         csvreader = csv.reader(csvfile, delimiter=',')
@@ -10,16 +10,34 @@ def get_input(adrs):
     return data
 
 
-def clean_data(data):
-    width = data[1]
+def clean_data(data: list) -> list:
+    weight = data[1]
     value = data[2]
-    width = list(map(float, width))
+    weight = list(map(float, weight))
     value = list(map(float, value))
-    data[1:] = width,value
+    data[1:] = weight, value
     return data
 
 
+class Thing:
+    def __init__(self, name, weight, value) -> None:
+        self.name = name
+        self.weight = weight
+        self.vlaue = value
+
+    def __str__(self) -> str:
+        return self.name
+
+
+# read and clean information from csv file
 address = 'Myignore/test.csv'
 data = get_input(address)
 data = clean_data(data)
-print(data)
+
+# make a list of objects and append object to it
+objects = list()
+for index in range(len(data[0])):
+    newOne = Thing(data[0][index], data[1][index], data[2][index])
+    objects.append(newOne)
+
+portableWeight = float(input('What is knopesack size (Kg): '))
