eabc4cea634568a8408e5079136a55d5b35afdb9
Sir-Sorg <sinaorojlo53@gmail.com>
2022-12-16 19:22:29 +0330

    read and clean data function append on the app

--- Kernel.py
+++ Kernel.py
@@ -1,4 +1,25 @@
 import csv
 
-def get_input(address):
-    
+
+def get_input(adrs):
+    data = list()
+    with open(adrs) as csvfile:
+        csvreader = csv.reader(csvfile, delimiter=',')
+        for row in csvreader:
+            data.append(row)
+    return data
+
+
+def clean_data(data):
+    width = data[1]
+    value = data[2]
+    width = list(map(float, width))
+    value = list(map(float, value))
+    data[1:] = width,value
+    return data
+
+
+address = 'Myignore/test.csv'
+data = get_input(address)
+data = clean_data(data)
+print(data)
